5caf7f3bd76db2ef265425bb454e6e6f772b3409
Damien008 <damien.lambert999@gmail.com>
2020-12-01 17:08:34 +0100

    quete 8 symfony récupérer des données stockées avec Doctrine

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -15,7 +15,7 @@ class DefaultController extends AbstractController
     public function index(): Response
     {
         return $this->render('index.html.twig', [
-        'website' => 'Bienvenue !',
+        'website' => 'Bienvenue sur Wild Series !',
         ]);
     }
 }
